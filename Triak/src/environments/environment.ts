ea429e23bc7f33b07d5f3b0e78619e354522b685
ardnahcivar <ravbhanage@gmail.com>
2018-06-23 08:07:29 +0530

    removed firebase credentials

--- Triak/src/environments/environment.ts
+++ Triak/src/environments/environment.ts
@@ -5,12 +5,7 @@
 
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyALh4oQfqy4VXDyKRCXB8--7_NNEK-Q0HY",
-    authDomain: "my-awesome-note.firebaseapp.com",
-    databaseURL: "https://my-awesome-note.firebaseio.com",
-    projectId: "my-awesome-note",
-    storageBucket: "my-awesome-note.appspot.com",
-    messagingSenderId: "1067273660013"
+    
   },
   production: true
 };
